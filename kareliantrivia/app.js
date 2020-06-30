6cd59b78e4df1a3c66a08da4403940550ddfaba0
Artem Vasilev <artem.vasilev@rwth-aachen.de>
2020-06-30 21:26:44 +0200

    Cleaning up the code. Being impressed with even the simplest things.

--- kareliantrivia/app.js
+++ kareliantrivia/app.js
@@ -7,14 +7,28 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content.startsWith(`${prefix}ping`)) {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
 		message.channel.send('Pong.');
-	} else if (message.content.startsWith(`${prefix}beep`)) {
+	} else if (command === 'beep') {
 		message.channel.send('Boop.');
-	} else if (message.content === `${prefix}server`) {
+	} else if (command === 'server') {
 		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${prefix}user-info`) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.avatarURL()}\nLast message sent: ${message.author.lastMessage}`);
+	} else if (command === 'userinfo') {
+		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.avatarURL({ format: "png", dynamic: true })}\nLast message sent: ${message.author.lastMessage}`);
+	} else if (command === 'kick') {
+		// grab the "first" mentioned user from the message
+		// this will return a `User` object, just like `message.author`
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to kick them!');
+		}
+		const taggedUser = message.mentions.users.first();
+
+		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
 	}
 });
 
